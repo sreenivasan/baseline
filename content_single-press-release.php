a5591491786b01dead71c44ef4447207389a7a41
matthew-350 <matthew@350.org>
2017-05-26 18:33:51 -0400

    change class prefix to match content type

--- content_single-press-release.php
+++ content_single-press-release.php
@@ -5,7 +5,7 @@
 */
 ?>
 <article class="press-release c10">
-		<p><span id="post-time" class="meta"><?php echo (get_the_date() ); ?></span></p>
+		<p><span id="press-release-time" class="meta"><?php echo (get_the_date() ); ?></span></p>
 		<h3><span class=""><?php the_title(); ?></span></h3>
 		<p>&nbsp;</p>
 		<?php the_content(); ?>
