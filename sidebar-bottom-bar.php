ea7960db6ed4786486ea565f650b3f236076b4a9
matthew-350 <matthew@350.org>
2017-05-31 12:34:08 -0400

    updated incorrect bottom bar ID

--- sidebar-bottom-bar.php
+++ sidebar-bottom-bar.php
@@ -1,5 +1,5 @@
 <?php if (is_active_sidebar('bottom-bar')) { ?> 
-<footer id="site-footer" class="section padding-large width-wide bg-dkgray-trans">
+<footer id="site-bottombar" class="section padding-large width-wide bg-dkgray-trans">
 	<div class="section-inner clearfix">
 		<?php dynamic_sidebar( 'bottom-bar' ); ?>
 		<br class="clear" />
