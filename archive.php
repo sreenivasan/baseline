dd3d56146059be9c31d51a740668d8a8a5f8b530
matthew-350 <matthew@350.org>
2017-05-31 12:26:18 -0400

    updating archive.php for custom post type archives
    updated so that it displays the CPT name instead of the slug, and
    restyled so the word "Archive:" is less prominent.

--- archive.php
+++ archive.php
@@ -16,7 +16,9 @@
 		$cat_id = get_query_var('tag');
 		$cat_name = single_tag_title( $prefix = '', $display = false ); 
 	} else if(is_post_type_archive()){
-		$cat_name = get_post_type();
+		$post_type = get_post_type();
+		$post_type_obj = get_post_type_object( $post_type );
+		$cat_name = $post_type_obj->labels->name;
 	}
 	//get taxonomy name
 	global $wp_query;
@@ -26,10 +28,9 @@
 
 <header id="archive-header" class="section padding-large bg-transparent width-wide">
 	<div class="section-inner" id="archive-header-inner">
+		<h4 class="margin-bottom-normal"><span class="text-highlight text-caps bg-dkgray-trans archive-label"><?php _e('Archive','baseline'); ?>:</span></h5>
 		<h2 id="page-title" class="title2">
-			<span class="<?php echo $text_on_image_classes; ?>">
-				<span class="archive-label"><?php _e('Archive','baseline'); ?>:</span> <?php echo $cat_name; ?>
-			</span>
+			<span class="<?php echo $text_on_image_classes; ?>"><?php echo $cat_name; ?></span>
 		</h2>
 		<?php if ( $cat_desc ): ?>
 		<p class="lead"><span class="<?php echo $text_on_image_classes; ?>"><?php echo $cat_desc; ?></span></p>
@@ -69,5 +70,4 @@
 </div>
 <?php endif; ?> 
 <!-- end #content -->
-<?php get_sidebar('featboxes'); ?>
 <?php get_footer(); ?>
