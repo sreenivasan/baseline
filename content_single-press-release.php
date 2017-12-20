eb459d4b9b4e9a794d4a9cdf4faabeb4a4e2894a
Matthew Anderson <matthew@350.org>
2017-12-20 10:11:34 -0500

    Added links to single press release view
    Added de-styled links to date and title, so people can still click through to the normal single press release.

--- content_single-press-release.php
+++ content_single-press-release.php
@@ -1,11 +1,15 @@
-<?php 
+<?php
 /*
 * Single Template for Single Press Releases
 *
 */
 ?>
 <article class="press-release c10">
-	<p><span id="press-release-time" class="meta"><?php echo (get_the_date() ); ?></span></p>
-	<h3 class="margin-bottom-large"><span class=""><?php the_title(); ?></span></h3>
+	<p id="press-release-time" class="meta">
+		<a class="text-unstyled text-underline-none" href="<?php the_permalink(); ?>"><?php echo (get_the_date() ); ?></a>
+	</p>
+	<h3 class="margin-bottom-large">
+		<a class="text-dark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	</h3>
 	<?php the_content(); ?>
 </article>
