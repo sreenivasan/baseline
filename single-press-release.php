3f6fc93324f8d58aa7a53068a6ec687649418853
Matthew Anderson <matthew@350.org>
2018-03-01 19:51:33 -0500

    adding the "text on image" feature to single press releases
    its absence must've been an oversight

--- single-press-release.php
+++ single-press-release.php
@@ -1,17 +1,21 @@
-<?php 
+<?php
 /*
 * Template for Single Press Releases
 *
 */
+
+// Text-on-image classes
+	$text_on_image_classes = get_option('site_text_on_image_classes');
+
 get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<?php 
+<?php
 
 ?>
 <div id="press-release-header" class="section width-narrow padding-medium bg-transparent">
 	<div class="section-inner">
 		<p><span id="post-time" class="meta"><?php echo (get_the_date() ); ?></span></p>
-		<h2 class="margin-bottom-large"><span class=""><?php the_title(); ?></span></h2>
+		<h2 class="margin-bottom-large"><span class="<?php echo $text_on_image_classes; ?>"><?php the_title(); ?></span></h2>
 	</div>
 </div>
 <section id="content" class="content section bg-white width-narrow padding-normal">
